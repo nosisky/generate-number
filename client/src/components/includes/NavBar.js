73736cf5361512b812c987b068981efc569b2520
Nosisky <nosisky@gmail.com>
2019-01-17 00:43:23 +0100

    Install dev dependency

--- client/src/components/includes/NavBar.js
+++ client/src/components/includes/NavBar.js
@@ -13,7 +13,7 @@ const NavBar = () => {
           <a className="nav-link" href="!#">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="!#">Features</a>
+          <a className="nav-link" href="http://github.com/nosisky/number-generator">Github</a>
         </li>
       </ul>
     </div>
