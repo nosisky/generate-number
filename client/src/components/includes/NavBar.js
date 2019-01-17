43c35ff8385aadded2ccbc7a73232a9c540057de
Nosisky <nosisky@gmail.com>
2019-01-17 02:38:54 +0100

    Fix test error

--- client/src/components/includes/NavBar.js
+++ client/src/components/includes/NavBar.js
@@ -13,7 +13,7 @@ const NavBar = () => {
           <a className="nav-link" href="!#">Home <span className="sr-only">(current)</span></a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="http://github.com/nosisky/number-generator">Github</a>
+          <a className="nav-link" href="http://github.com/nosisky/generate-number">Github</a>
         </li>
       </ul>
     </div>
